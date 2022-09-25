704c20208db4028dc8d8f8aedfe81e6c38705c96
Cliff Choiniere <crcnum4@gmail.com>
2022-09-25 16:27:56 -0400

    add id declaration

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+use solana_program::{
+  entrypoint::ProgramResult, 
+  program_error::ProgramError, 
+  pubkey::Pubkey
+};
+
 pub mod processor;
 pub mod error;
 pub mod instruction;
@@ -9,3 +15,12 @@ pub mod unpack;
 
 #[cfg(not(feature = "no-entrypoint"))]
 pub mod entrypoint;
+
+solana_program::declare_id!("Epf86va8B3wDCJ2t47nV6EVT9fVipnnTbLihtLZiL7am");
+
+pub fn check_program_account(ree_meta_program_id: &Pubkey) -> ProgramResult {
+  if ree_meta_program_id != &id() {
+    return Err(ProgramError::IncorrectProgramId);
+  }
+  Ok(())
+}
